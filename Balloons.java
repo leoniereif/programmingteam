0491e0f56bd7f3b019839baf47c055b4c7f682dd
Leonie <leonie.reif@gatech.edu>
2015-12-28 22:48:33 +0100

    sbt changes

--- Balloons.java
+++ Balloons.java
@@ -8,17 +8,17 @@ public class Balloons {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int[][] array = new int[n][3];
-        for(int i = 0; i<n; i++) {
+        for (int i = 0; i<n; i++) {
             int k = scanner.nextInt();
             int toa = scanner.nextInt();
             int tob = scanner.nextInt();
-            array[i][0]=k*toa;
-            array[i][1]=k*tob;
-            array[i][2]=k;
+            array[i][0] = k * toa;
+            array[i][1] = k * tob;
+            array[i][2] = k;
         }
         int sum = 0;
-        for(int i = 0; i<n; i++) {
-            if(array[i][0]<array[i][0]) {
+        for (int i = 0; i<n; i++) {
+            if (array[i][0] < array[i][0]) {
                 sum += array[i][0];
             } else {
                 sum += array[i][1];
